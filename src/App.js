b655f75cf44d5677e7d96122f1fb740fd61c6be6
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-29 15:41:36 +0100

    pie chart working with dummy data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Chart from './components/Chart';
 
 function App() {
+  const [user] = useState({
+    name: 'User1',
+    footprintData: {
+      vacations: 100,
+      transport: 200,
+      diet: 300,
+      personal: 400,
+      energy: 500
+    }
+  })
+  const [chartOptions, setChartOptions] = useState(null)
+
+  useEffect(() => {
+    getChartOptions()
+  }, [user])
+
+  const getChartOptions = function() {
+      const options = {
+        chart: {
+        type: 'pie'
+        },
+        title: {
+          text: 'CO<span class="highcharts-title subscript">2</span> footprint by category'
+        },
+        tooltip: {
+          pointFormat: '<b>{point.y}</b> CO2'
+        },
+        plotOptions: {
+          pie: {
+            dataLabels: {
+              enabled: true,
+              format: '{point.name}: {point.percentage:.1f}%'
+            }
+          }
+        },
+        series: [
+          {
+            data: [
+              {
+                name: 'Vacations',
+                y: user.footprintData.vacations
+              },
+              {
+                name: 'Transport',
+                y: user.footprintData.transport
+              },
+              {
+                name: 'Diet',
+                y: user.footprintData.diet
+              },
+              {
+                name: 'Personal consumption',
+                y: user.footprintData.personal
+              },
+              {
+                name: 'Domestic energy',
+                y: user.footprintData.energy
+              }
+            ]
+          }
+        ]
+      };
+      setChartOptions(options);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart footprintData={user.footprintData} chartOptions={chartOptions} />
     </div>
   );
 }
