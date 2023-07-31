54cb9d6b1dc66c7d3ee6e4cd39cd81ed270ba01b
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-31 12:10:10 +0100

    added button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Button from './components/Button';
 import ResultsContainer from './components/ResultsContainer';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>CO<span className='subscript'>2</span> tracker</h1>
+      </header>
       <ResultsContainer />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button() {
+    return (
+        <button>Retake questionnaire</button>
+    )
+}
+
+export default Button;
