72cfbf077ecfb15e0f5a68ad6edf235f7b971e96
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-31 12:30:54 +0100

    added header and styled button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    font-size: 1em;
+    padding: 0.7em;
+    border: none;
+    background-color: rgb(84, 79, 197);
+    color: white;
+    font-weight: bold;
+    &:hover {
+        background-color: lightgreen;
+        color: black;
+    }
+`
 
 function Button() {
     return (
-        <button>Retake questionnaire</button>
+        <StyledButton>Retake questionnaire</StyledButton>
     )
 }
 
