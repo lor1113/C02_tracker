2a6dc63d61df9ab66e38e5cec4fbde12ca55dda3
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-31 11:42:04 +0100

    FootprintDetail displays dummy data

--- src/components/FootprintDetail.js
+++ src/components/FootprintDetail.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function FootprintDetail({footprintData}) {
+    const values = Object.values(footprintData);
+    const totalFootprint = values.reduce((accumulator, value) => {
+        return accumulator + value;
+    }, 0);
+
     return (
         <>
-            <h3>My carbon footprint: </h3>
+            <h3>My carbon footprint: {totalFootprint}</h3>
             <h3>World average: </h3>
             <h3>Group average: </h3>
         </>
