4bdf182629e5b042039818214bbe62c1736641dd
Kagke <gianniskagkelis@gmail.com>
2023-07-31 13:23:30 +0300

    setted up

---
+++ server/db/seeds.js
@@ -0,0 +1,150 @@
+use users
+
+db.co2Tracker.insertMany([
+    {
+        name: "Giannis",
+        email: "giannis@hotmail.com",
+        password: "giannis123",
+        answears:{
+        vacationAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        transportAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        dietAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        personalConsumptionAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        domesticEnergyAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }}
+        ,
+      },
+      {
+        name: "Saul",
+        email: "saulmackay@gmail.com",
+        password: "saul123",
+        answears:{
+            vacationAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        transportAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        dietAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        personalConsumptionAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        domesticEnergyAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        }
+        ,
+      },
+      {
+        name: "Aurelija",
+        email: "aurelijabelerby@yahoo.com",
+        password: "aurelija123",
+        answears:{
+        vacationAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        transportAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        dietAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        personalConsumptionAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        domesticEnergyAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        }
+      },
+      {
+        name: "Lorenzo",
+        email: "lorenzo@hotmail.com",
+        password: "lorenzo123",
+        answears:{
+        vacationAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        transportAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        dietAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        personalConsumptionAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        ,
+        domesticEnergyAnsw:
+            {
+            q1:"answear1",
+            q2:"answear2",
+            }
+        }
+      },
+      
+])
