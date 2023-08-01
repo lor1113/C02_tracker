5b29d003a180c3ce29a1fef49d37a72f4e3c9493
Lorenzo Curcio <Lorenzocurcio2@gmail.com>
2023-08-01 15:44:14 +0200

    added show difference functionality

--- src/components/SubQuestionnaire.js
+++ src/components/SubQuestionnaire.js
@@ -1,8 +1,11 @@
-function SubQuestionnaire ({dropdownClick, dropdown, dropdownNum, SubForm, formText, handleOnChange, formValues, isCompleted}) {
+import SubQuestionnaireDiff from "./SubQuestionnaireDiff";
+
+function SubQuestionnaire ({dropdownClick, tempResult, prevResult, dropdown, dropdownNum, SubForm, formText, handleOnChange, formValues, isCompleted}) {
     return(
         <div className="formDiv">
             <span className="formSpan" onClick = {(event) => dropdownClick(event, (dropdown === dropdownNum?0:dropdownNum))}>
-                 <h3>{formText}</h3>
+                 <h3 className="formTitle">{formText}</h3>
+                 <h3>{prevResult !== 0 ? <SubQuestionnaireDiff tempResult={tempResult} prevResult={prevResult}/> : <></>}</h3>
                  <h3>{isCompleted ? <span className="green">Completed!</span> : <span className="red">Not Completed.</span>}</h3>
             </span>
             {dropdown === dropdownNum?<SubForm handleOnChange={handleOnChange} formValues={formValues}/>:<></>}
