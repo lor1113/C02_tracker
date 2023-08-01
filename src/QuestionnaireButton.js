f5bc08d244bd9471368f5c7c397c62a8d1eb727d
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-08-01 15:05:51 +0100

    added styling & footprint %

--- src/QuestionnaireButton.js
+++ src/QuestionnaireButton.js
@@ -1,21 +1,6 @@
-import styled from "styled-components";
-
-const StyledButton = styled.button`
-    font-size: 1em;
-    padding: 0.7em;
-    border: none;
-    background-color: rgb(84, 79, 197);
-    color: white;
-    font-weight: bold;
-    &:hover {
-        background-color: lightgreen;
-        color: black;
-    }
-`
-
 const QuestionnaireButton = ({handleSubmit,allCompleted}) => {
     return (
-        <StyledButton onClick={handleSubmit} className="questionnaireButton" type="button" disabled={!allCompleted}>See my results!</StyledButton>
+        <button onClick={handleSubmit} className="questionnaireButton" disabled={!allCompleted}>See my results!</button>
     )
     
 }
