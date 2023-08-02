062079156b7fe782a286e3659013194392ff6c7d
Sauzlo <saul.mackay@googlemail.com>
2023-08-02 11:47:57 +0100

    Added another test

--- client/cypress/e2e/questionnaire.cy.js
+++ client/cypress/e2e/questionnaire.cy.js
@@ -3,7 +3,7 @@ describe("Questionnaire", ()=>{
         cy.visit("http://localhost:3000")
     })
 
-    it('should connect to cypress', ()=>{
+    it('should connect to server', ()=>{
     })
 
     it('should not submit if form not complete', ()=>{
@@ -31,4 +31,30 @@ describe("Questionnaire", ()=>{
         cy.get('button').should('be.enabled')
         cy.get('button').click()
     })
+
+    it('should allow for questionnaire to be resubmitted', ()=>{
+        cy.get('#vacationCount').type('3')
+        cy.get('#vacationDistance').select('country')
+        cy.get('#vacationTravel').select('car')
+        cy.get('#1.formDiv').click()
+        cy.get('#transportType').select('car')
+        cy.get('#travelCount').type('4')
+        cy.get('#2.formDiv').click()
+        cy.get('#dietType').select('omnivore')
+        cy.get('#dietLocal').select('mid')
+        cy.get('#3.formDiv').click()
+        cy.get('#consumptionClothes').select('fewMonths')
+        cy.get('#consumptionElectronics').select('fewYears')
+        cy.get('#consumptionSustainability').select('somewhat')
+        cy.get('#4.formDiv').click()
+        cy.get('#heatingCount').type('4')
+        cy.get('#coolingCount').type('3')
+        cy.get('#energyElectricity').select('entirely')
+        cy.get('button').click()
+        cy.get('button').click()
+        cy.get('#vacationTravel').select('plane')
+        cy.get('button').click()
+    })
+
+
 })
